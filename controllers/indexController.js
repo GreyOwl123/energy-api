83b0de20a79faed8058a4db263ab8ad6c98cfbf9
Muhammed Ibrahim <muib13579@gmail.com>
2023-08-23 12:16:04 +0100

    New

---
+++ controllers/indexController.js
@@ -0,0 +1,12 @@
+const object = require("../models/indexObject");
+
+
+// Api get Object functions
+exports.object_detail = {}
+
+// Create, Update and Delete functions for Admin only
+exports.object_create 
+
+exports.object_update
+
+exports.object_delete
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const index_controller = require("../controllers/indexController");
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/object/:id', index_controller.object_detail);
+
+router.post('/object/create', index_controller.object_create);
+
+router.post('/object/:id/update', index_controller.object_update);
+
+router.post('/object/:id/delete', index_controller.object_delete);
+
 
 module.exports = router;
