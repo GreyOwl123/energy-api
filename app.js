8b2ee3d676f450ebb59cc7d2e09573b71abf7bbe
Muhammed Ibrahim <muib13579@gmail.com>
2023-09-26 11:27:33 +0100

    created mongoDB database

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
+if (process.env.NODE_ENV !== "production") {
+  require('dotenv').config();
+}
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require("cors");
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./routes/api');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// Mongoose connection
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = process.env.MONGO_DB_URL;
+
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public/htmls')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/htmls', 'index.html')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
